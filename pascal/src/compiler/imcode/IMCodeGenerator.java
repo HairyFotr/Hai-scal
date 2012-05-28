f73cee4862f3486ebe837435fb01a68c7c5a9b8f
HairyFotr <hairyfotr@gmail.com>
2012-05-28 15:00:54 +0200

    DN6 (some b0rken stuff)

--- pascal/src/compiler/imcode/IMCodeGenerator.java
+++ pascal/src/compiler/imcode/IMCodeGenerator.java
@@ -3,7 +3,9 @@ package compiler.imcode;
 import compiler.abstree.*;
 import compiler.abstree.tree.*;
 import compiler.semanal.*;
+import compiler.semanal.type.*;
 import compiler.frames.*;
+import compiler.imcode.*;
 import java.util.*;
 
 /*Sestavite generator vmesne kode, ki izracuna fragmente vmesne kode:
@@ -37,41 +39,118 @@ P.S. Dodeljevanje pomnilnika z izrazom oblike [<type>] prevedete v klic funkcije
 public class IMCodeGenerator implements AbsVisitor {
 	
 	/** Zaporedje delov kode.  */
-	public static LinkedList<ImcChunk> chunks;
+	public static LinkedList<ImcChunk> chunks = new LinkedList<ImcChunk>();
+	ImcCode code;
+	boolean mem = true;
 
-    boolean funcCall = false;
-    int sizeArgs = 0;
-
-    //
+    public void visit(AbsAlloc acceptor) {
+        ImcCALL malloc = new ImcCALL(FrmLabel.newLabel("malloc"));
+        int argSize = SemDesc.getActualType(acceptor.type).size();
+        malloc.args.add(new ImcCONST(argSize));
+        code = (ImcCode)malloc;
+    }
+    
     public void visit(AbsAtomType acceptor) {}
     
+    public void visit(AbsAtomConst acceptor) {
+        int val = 0;
+        switch(acceptor.type) {
+            case AbsAtomConst.BOOL: val = acceptor.value.equals("true")? 1 : 0; break;
+            case AbsAtomConst.CHAR: val = (int)acceptor.value.charAt(1); break;
+            case AbsAtomConst.INT:  val = Integer.parseInt(acceptor.value); break;         
+        }
+        code = new ImcCONST(val);
+    }
+    public void visit(AbsUnExpr acceptor) { 
+        acceptor.expr.accept(this);
+        
+        switch(acceptor.oper) {
+            case AbsUnExpr.ADD: break;//nop 
+            case AbsUnExpr.SUB: code = new ImcBINOP(ImcBINOP.SUB, (ImcExpr)(new ImcCONST(0)), (ImcExpr)code); break;
+            case AbsUnExpr.NOT: code = new ImcBINOP(ImcBINOP.NEQ, (ImcExpr)code, (ImcExpr)code); break;
+            case AbsUnExpr.MEM: code = ((ImcMEM)code).expr; break;
+            case AbsUnExpr.VAL: code = new ImcMEM((ImcExpr)code); break;
+        }
+    }
+    
+    public void visit(AbsBinExpr acceptor) {
+        switch(acceptor.oper) {
+            case AbsBinExpr.RECACCESS:
+                mem = false;
+                acceptor.fstExpr.accept(this);
+                ImcExpr record = (ImcExpr)code;
+                SemRecordType recordType = (SemRecordType)SemDesc.getActualType(acceptor.fstExpr);
+                String member = ((AbsValName)acceptor.sndExpr).name;
+                
+                SemType memberType = null;
+                int offset = 0;
+                for(int i=0; i<recordType.getNumFields(); i++) {
+                    if(member.equals(recordType.getFieldName(i).name)) {
+                        memberType = recordType.getFieldType(i);
+                        break;
+                    }
+                    offset += recordType.getFieldType(i).size();
+                }
+                
+                if(memberType instanceof SemRecordType) 
+                    code = new ImcBINOP(ImcBINOP.ADD, record, new ImcCONST(offset));
+                else
+                    code = new ImcMEM(new ImcBINOP(ImcBINOP.ADD, record, new ImcCONST(offset)));
+                
+                mem = true;
+            break;
+            case AbsBinExpr.ARRACCESS:
+                mem = false;
+                acceptor.fstExpr.accept(this);
+                ImcExpr array = (ImcExpr)code;
+                SemArrayType arrayType = (SemArrayType)SemDesc.getActualType(acceptor.fstExpr);
+                
+                mem = true;
+                acceptor.fstExpr.accept(this);
+                ImcExpr index = (ImcExpr)code;
+                mem = false;
+
+                ImcBINOP arrIndex = new ImcBINOP(ImcBINOP.SUB, index, new ImcCONST(arrayType.loBound));
+                ImcBINOP arrOffset = new ImcBINOP(ImcBINOP.MUL, arrIndex, new ImcCONST(arrayType.type.size()));
+
+                code = new ImcMEM(new ImcBINOP(ImcBINOP.ADD, array, arrOffset));
+                mem = true;
+            break;            
+            default:
+                acceptor.fstExpr.accept(this);
+                ImcExpr fstExpr = (ImcExpr)code;
+                acceptor.sndExpr.accept(this);
+                ImcExpr sndExpr = (ImcExpr)code;
+                
+                code = new ImcBINOP(acceptor.oper, fstExpr, sndExpr);
+            break;
+        }        
+    }
+
     public void visit(AbsConstDecl acceptor) {}
     
-    public void visit(AbsFunDecl acceptor) {
-        for (AbsDecl decl : acceptor.decls.decls) {
-            decl.accept(this);
-        }
-        for (AbsDecl decl : acceptor.pars.decls) {
-            decl.accept(this);
-        }
-        acceptor.stmt.accept(this);
-    }
-    
     public void visit(AbsProgram acceptor) {
-        for (AbsDecl decl : acceptor.decls.decls) {
-            decl.accept(this);
+        for (AbsDecl decl : acceptor.decls.decls) if (decl instanceof AbsVarDecl) {//global vars
+            AbsVarDecl varDecl = (AbsVarDecl)decl;
+            FrmVarAccess varAccess = (FrmVarAccess)FrmDesc.getAccess(varDecl);
+            SemType varType = SemDesc.getActualType(varDecl.type);
+            chunks.add(new ImcDataChunk(varAccess.label, varType.size()));
         }
+        acceptor.decls.accept(this);//funcs
+
         acceptor.stmt.accept(this);//main
+        chunks.add(new ImcCodeChunk(FrmDesc.getFrame(acceptor), (ImcStmt)code));
     }
     
-    public void visit(AbsProcDecl acceptor) {
-        for (AbsDecl decl : acceptor.decls.decls) {
-            decl.accept(this);
-        }
-        for (AbsDecl decl : acceptor.pars.decls) {
-            decl.accept(this);
-        }
+    public void visit(AbsFunDecl acceptor) {
+        acceptor.decls.accept(this);
         acceptor.stmt.accept(this);
+        chunks.add(new ImcCodeChunk(FrmDesc.getFrame(acceptor), (ImcStmt)code));
+    }
+    public void visit(AbsProcDecl acceptor) {
+        acceptor.decls.accept(this);
+        acceptor.stmt.accept(this);
+        chunks.add(new ImcCodeChunk(FrmDesc.getFrame(acceptor), (ImcStmt)code));
     }
     
     public void visit(AbsRecordType acceptor) {
@@ -84,58 +163,147 @@ public class IMCodeGenerator implements AbsVisitor {
 
     public void visit(AbsVarDecl acceptor) { acceptor.type.accept(this); }
 
-    public void visit(AbsAlloc acceptor) { acceptor.type.accept(this); }
-
     public void visit(AbsArrayType acceptor) { acceptor.type.accept(this); }
 
     public void visit(AbsAssignStmt acceptor) {
         acceptor.dstExpr.accept(this);
+        ImcExpr dst = (ImcExpr)code;
         acceptor.srcExpr.accept(this);
-    }
-
-    public void visit(AbsAtomConst acceptor) {}
-
-    public void visit(AbsBinExpr acceptor) {
-        acceptor.fstExpr.accept(this);
-        acceptor.sndExpr.accept(this);
+        ImcExpr src = (ImcExpr)code;
+        
+        code = new ImcMOVE(dst, src);
     }
 
     public void visit(AbsBlockStmt acceptor) { acceptor.stmts.accept(this); }
 
     public void visit(AbsCallExpr acceptor) {
+        FrmFrame frm = FrmDesc.getFrame(SemDesc.getNameDecl(acceptor.name));
+        ImcCALL call = new ImcCALL(frm.label);
+        call.args.add(new ImcTEMP(frm.FP));
+        for(AbsValExpr expr : acceptor.args.exprs) {
+            expr.accept(this);
+            call.args.add((ImcExpr)code);
+        }
+        code = call;
     }
 
     public void visit(AbsDeclName acceptor) {}
 
-    public void visit(AbsDecls acceptor) { for (AbsDecl decl : acceptor.decls) decl.accept(this); }
+    public void visit(AbsDecls acceptor) { 
+        for (AbsDecl decl : acceptor.decls) 
+            if(decl instanceof AbsFunDecl || decl instanceof AbsProcDecl)
+                decl.accept(this);
+    }
 
-    public void visit(AbsExprStmt acceptor) { acceptor.expr.accept(this); }
+    public void visit(AbsExprStmt acceptor) { 
+        acceptor.expr.accept(this);
+        code = new ImcEXP((ImcExpr)code);
+    }
 
     public void visit(AbsForStmt acceptor) {
+        ImcSEQ seq = new ImcSEQ();
         acceptor.name.accept(this);
+        ImcExpr name = (code instanceof ImcEXP)?((ImcEXP)code).expr:(ImcExpr)code;
         acceptor.loBound.accept(this);
+        ImcExpr lo = (ImcExpr)code;
         acceptor.hiBound.accept(this);
+        ImcExpr hi = (ImcExpr)code;
+        
+        ImcLABEL tl = new ImcLABEL(FrmLabel.newLabel());
+        ImcLABEL fl = new ImcLABEL(FrmLabel.newLabel());
+        ImcLABEL sl = new ImcLABEL(FrmLabel.newLabel());
+        
+        seq.stmts.add(new ImcMOVE(name, lo));
+        seq.stmts.add(sl);
+            seq.stmts.add(new ImcCJUMP(new ImcBINOP(ImcBINOP.LEQ, name, hi), tl.label, fl.label));
+        seq.stmts.add(tl);
+            acceptor.stmt.accept(this);
+            seq.stmts.add((ImcStmt)code);
+            seq.stmts.add(new ImcMOVE(new ImcBINOP(ImcBINOP.ADD, name, new ImcCONST(1)), name));
+            seq.stmts.add(new ImcJUMP(sl.label));
+        seq.stmts.add(fl);
+        
+        code = seq;
     }
 
     public void visit(AbsIfStmt acceptor) {
+        ImcSEQ seq = new ImcSEQ();
+        
         acceptor.cond.accept(this);
-        acceptor.thenStmt.accept(this);
-        acceptor.elseStmt.accept(this);
+        ImcExpr cond = (ImcExpr)code;
+        
+        ImcLABEL tl = new ImcLABEL(FrmLabel.newLabel());
+        ImcLABEL fl = new ImcLABEL(FrmLabel.newLabel());
+        ImcLABEL el = new ImcLABEL(FrmLabel.newLabel());
+        
+        seq.stmts.add(new ImcCJUMP(cond, tl.label, fl.label));
+        seq.stmts.add(tl);
+            acceptor.thenStmt.accept(this);
+            seq.stmts.add((ImcStmt)code);
+            seq.stmts.add(new ImcJUMP(el.label));
+        seq.stmts.add(fl);
+            acceptor.elseStmt.accept(this);
+            seq.stmts.add((ImcStmt)code);
+        seq.stmts.add(el);
+        
+        code = seq;
     }
 
-    public void visit(AbsNilConst acceptor) {}
+    public void visit(AbsNilConst acceptor) {
+        code = new ImcCONST(0);
+    }
 
     public void visit(AbsPointerType acceptor) { acceptor.type.accept(this); }
 
-    public void visit(AbsStmts acceptor) { for (AbsStmt s : acceptor.stmts) s.accept(this); }
+    public void visit(AbsStmts acceptor) { 
+        ImcSEQ stmts = new ImcSEQ();
+        for (AbsStmt stmt : acceptor.stmts) {
+            stmt.accept(this);
+            stmts.stmts.add((ImcStmt)code);
+        }
+        code = stmts;
+    }
 
     public void visit(AbsTypeName acceptor) {}
 
-    public void visit(AbsUnExpr acceptor) { acceptor.expr.accept(this); }
-
     public void visit(AbsValExprs acceptor) { for (AbsValExpr expr : acceptor.exprs) expr.accept(this); }
 
-    public void visit(AbsValName acceptor) {}
+    public void visit(AbsValName acceptor) {
+        AbsDecl decl = SemDesc.getNameDecl(acceptor);
+        FrmAccess access = FrmDesc.getAccess(decl);
+        
+        if(access instanceof FrmVarAccess) {
+            FrmVarAccess varAccess = (FrmVarAccess)access;
+            code = new ImcNAME(varAccess.label);
+            if(mem) code = new ImcMEM((ImcNAME)code);
+        }
+        if(access instanceof FrmArgAccess) {
+            FrmArgAccess argAccess = (FrmArgAccess)access;
+            code = new ImcBINOP(ImcBINOP.ADD, new ImcTEMP(argAccess.frame.FP), new ImcCONST(argAccess.offset));
+            if(mem) code = new ImcMEM((ImcBINOP)code);
+        }
+        if(access instanceof FrmLocAccess) {
+            FrmLocAccess locAccess = (FrmLocAccess)access;
+            code = new ImcBINOP(ImcBINOP.ADD, new ImcTEMP(locAccess.frame.FP), new ImcCONST(locAccess.offset));
+            if(mem) code = new ImcMEM((ImcBINOP)code);
+        }
+        
+        if(decl instanceof AbsFunDecl) {
+            FrmFrame frm = FrmDesc.getFrame(decl);
+            code = new ImcTEMP(frm.RV);
+            if(mem) code = new ImcMEM((ImcTEMP)code);
+            
+            SemType type = SemDesc.getActualType(decl);
+            if(type instanceof SemRecordType || type instanceof SemArrayType) {
+                code = new ImcMEM((ImcExpr)code);
+            }
+        } else
+        if(decl instanceof AbsConstDecl) {
+            code = new ImcCONST(SemDesc.getActualConst(decl));
+        } else {
+            //code = new ImcEXP((ImcExpr)code);
+        }
+    }
 
     public void visit(AbsWhileStmt acceptor) {
         acceptor.cond.accept(this);
